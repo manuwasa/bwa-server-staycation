07f89b29e42b97e3a4ddc608ac79196f452304fe
manuwasa <manuwasa.f@gmail.com>
2021-10-05 18:22:54 +0700

    Full project staycation

---
+++ test/index.js
@@ -0,0 +1,26 @@
+const chai = require("chai");
+const chaiHttp = require("chai-http");
+const expect = chai.expect;
+const app = require("../app");
+
+chai.use(chaiHttp);
+
+describe("API Endpoint Testing", () => {
+  it("GET Landing Page", (done) => {
+    chai
+      .request(app)
+      .get("/api/v1/member/landing-page")
+      .end((err, res) => {
+        expect(err).to.be.null;
+        expect(res).to.have.status(200);
+        expect(res.body).to.be.an("Object");
+        expect(res.body).to.have.property("hero");
+        expect(res.body.hero).to.have.all.keys(
+          "travelers",
+          "treasures",
+          "cities"
+        );
+        done();
+      });
+  });
+});
